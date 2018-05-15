d69e78b50ee6a108a88e384ba99f3ee9d8702f28
Caio Moreira Gomes <cmg@ug2c07.windows.cin.ufpe.br>
2018-05-15 19:07:00 -0300

    Adding the bases of the modules to generate

---
+++ partControl.v
@@ -0,0 +1,11 @@
+module partControl (
+	input clk,    // Clock
+	input clk_en, // Clock Enable
+	input rst_n,  // Asynchronous reset active low
+	input ignore_amt   // Defines the amount of numbers to ignore
+);
+
+// Control wich part of the word is going to pass the other part is going to become zero
+
+
+endmodule
