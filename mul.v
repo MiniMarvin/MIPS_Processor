584819866de5150cef0fb00431f33de7857b6bd5
Caio Gomes <caiogomes34@gmail.com>
2018-05-14 22:23:17 -0300

    Adding basic files to the project

---
+++ mul.v
@@ -0,0 +1,13 @@
+module mul (
+	input clk,       // Clock
+	input clk_en,    // Clock Enable
+	input rst_n,     // Asynchronous reset active low
+	input module_en, // Enable the operation of the module
+	input start_div, // Start the 
+	output ready,    // Inform that no division is occurring
+	output quotient, // Quotient of the division
+	output rest      // Rest of the division value
+	
+);
+
+endmodule
