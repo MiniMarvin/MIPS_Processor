b2707f054e3a3a8f1f2452eea250befc13931ed5
Caio Moreira Gomes <cmg@ug2c07.windows.cin.ufpe.br>
2018-05-15 18:50:35 -0300

    Putting the project to 13.0

---
+++ divMul.v
@@ -0,0 +1,16 @@
+module divMul (
+	input clk,    // Clock
+	input clk_en, // Clock Enable
+	input rst_n,  // Asynchronous reset active low
+	input divMul,  // Asynchronous reset active low
+	output ready,
+	output reg hi,
+	output reg lo
+);
+
+
+// The magic box to div/mul
+
+
+
+endmodule
